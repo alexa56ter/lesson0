b23a2bf16d75242a00105f229c97a36238fe854c
alexa56ter <leksa.55@mail.ru>
2024-05-15 17:54:39 +0300

    Add files via upload

---
+++ dz3.py
@@ -0,0 +1,8 @@
+my_string='Я люблю этот мир'
+print(my_string)
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[:1])
+print(my_string[-1])
