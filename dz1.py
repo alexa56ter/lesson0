cd1e59a5d505bac49428fa7edddafc3b02ee5e77
alexa56ter <leksa.55@mail.ru>
2024-05-17 18:03:25 +0300

    Add files via upload

--- dz1.py
+++ dz1.py
@@ -1,16 +1,14 @@
 name ='''"Сейчас на Земле появился новый вид роботов.Раньше их называли "железной оравой", но это не очень точное определение"'''
 print(name[1:44])
-name1='Палиндром'
-print(name1[::-1])
 name2='Радар'
 print(name2[::-1])
 name3='норма'
 print(name3[::-1])
-print(len(name))
+#print(len(name))
 print(name[58:117] + name[1:58])
 name4 = '"Нейропрограммирование"'
-print(len(name4))
+#print(len(name4))
 print(name4[1::2] , name4[2:22:2])
 name5='"Нейропластичность"'
-print(len(name5))
+#print(len(name5))
 print(name5[16:1:-1])
