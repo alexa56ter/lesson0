da7d9730956e99933031b7e9d83d460cd6a60d49
alexa56ter <leksa.55@mail.ru>
2024-05-17 15:59:04 +0300

    Add files via upload

---
+++ dz5.py
@@ -0,0 +1,15 @@
+my_list=['apple','banana','orange','kiwi']
+print('List',(my_list))
+print ('First element:', (my_list[1]))
+print('Last elenent:', (my_list[-1]))
+print('Sablist:', (my_list[2:4]))
+my_list[2]='peach'
+print('Modified list:', (my_list))
+
+
+my_dict = { 'Grape': 'виноград', 'Coconut':'кокос', 'Rain':'дождь', 'Sun': 'солнце'}
+print('Dictionaly:', (my_dict))
+print('Translation:', (my_dict.get('Sun')))
+my_dict['Grape']='Виноград'
+my_dict.update({'World': 'мир'})
+print('Modified dictionary:' ,(my_dict))
